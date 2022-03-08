aadd70820f32aaf29be237e44cc76f6ba85c1a9d
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-08 10:45:59 +0100

    Configured SonarQube coverage

--- src/App.test.js
+++ src/App.test.js
@@ -5,9 +5,11 @@ describe("Given an App component", () => {
   describe("When it renders", () => {
     test("Then it should display a heading with Welcome to PK Spots!", () => {
       render(<App />);
+
       const title = screen.getByRole("heading", {
         name: /welcome to pk spots!/i,
       });
+
       expect(title).toBeInTheDocument();
     });
   });
