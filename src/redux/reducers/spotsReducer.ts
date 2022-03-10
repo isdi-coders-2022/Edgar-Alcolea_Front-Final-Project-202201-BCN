51ea6a2152b3dda60eb511ae960b892147671be5
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-10 10:30:52 +0100

    Created spotsReducer

---
+++ src/redux/reducers/spotsReducer.ts
@@ -0,0 +1,23 @@
+import { AnyAction } from "redux";
+import SpotInterface from "../../types/SpotInterface";
+import actionTypes from "../actions/actionTypes";
+
+const spotsReducer = (
+  currentSpots: SpotInterface[] = [],
+  action: AnyAction
+): Array<SpotInterface> => {
+  let newSpots: SpotInterface[];
+
+  switch (action.type) {
+    case actionTypes.loadSpots:
+      newSpots = action.spots;
+      break;
+
+    default:
+      newSpots = [...currentSpots];
+      break;
+  }
+  return newSpots;
+};
+
+export default spotsReducer;
