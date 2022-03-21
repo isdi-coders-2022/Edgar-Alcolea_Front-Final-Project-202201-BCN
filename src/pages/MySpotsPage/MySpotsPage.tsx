f4e97de06f15e0cd38545755d620a5b31db52bec
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-21 12:13:54 +0100

    Added loadUserSpotsThunk to MySpotsPage.tsx

--- src/pages/MySpotsPage/MySpotsPage.tsx
+++ src/pages/MySpotsPage/MySpotsPage.tsx
@@ -4,20 +4,17 @@ import Button from "../../components/Button/Button";
 import Loading from "../../components/Loading/Loading";
 import SpotComponent from "../../components/SpotComponent/SpotComponent";
 import { useAppSelector } from "../../redux/hooks";
-import { loadSpotsThunk } from "../../redux/thunks/spotsThunks";
+import { loadUserSpotsThunk } from "../../redux/thunks/spotsThunks";
 
 const MySpotsPage = (): JSX.Element => {
   const dispatch = useDispatch();
   const spots = useAppSelector((state) => state.spots);
-  const user = useAppSelector((state) => state.user);
   const spotsPerPage = 2;
   const [visibleSpots, setVisibleSpots] = useState(spotsPerPage);
 
   useEffect(() => {
-    dispatch(loadSpotsThunk);
+    dispatch(loadUserSpotsThunk);
   }, [dispatch]);
-
-  const mySpots = spots.filter((spot) => spot.createdBy.id === user.id);
 
   const loadMore = (): void => {
     setVisibleSpots((prevValue) => prevValue + spotsPerPage);
@@ -26,12 +23,12 @@ const MySpotsPage = (): JSX.Element => {
   return (
     <>
       <h1 className="page-title">My Spots</h1>
-      {mySpots.length === 0 && <Loading />}
+      {spots.length === 0 && <Loading />}
       <ul className="spot-list">
-        {mySpots.slice(0, visibleSpots).map((spot) => (
+        {spots.slice(0, visibleSpots).map((spot) => (
           <SpotComponent key={spot.id} spot={spot} />
         ))}
-        {mySpots.length > visibleSpots && <Button actionOnClick={loadMore} />}
+        {spots.length > visibleSpots && <Button actionOnClick={loadMore} />}
       </ul>
     </>
   );
